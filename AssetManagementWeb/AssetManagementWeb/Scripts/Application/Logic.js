a60547f7b82819959afceaddf6bb18c980188caf
Jalmari Talvipuro <jalmari.talvipuro@stud.pointcollege.fi>
2018-12-08 23:52:20 +0200

    Typescript, test.cshtml

---
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
@@ -0,0 +1,7 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
+function initAssetAssignment() {
+    $("#AssignAssetButton").click(function () {
+        alert("Jdenfrei");
+    });
+}
+//# sourceMappingURL=Logic.js.map
---
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+
+function initAssetAssignment() {
+	$("#AssignAssetButton").click(function () {
+		alert("Jdenfrei");
+	});
+}
