8722c4e962f5b62cf01dd02d35beae3ca079b3b2
Jalmari Talvipuro <jalmari.talvipuro@stud.pointcollege.fi>
2018-12-09 01:30:00 +0200

    Ei toimi

--- AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
@@ -1,7 +1,33 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
+var AssignLocationModel = /** @class */ (function () {
+    function AssignLocationModel() {
+    }
+    return AssignLocationModel;
+}());
 function initAssetAssignment() {
     $("#AssignAssetButton").click(function () {
-        alert("Jdenfrei");
+        var locationCode = $("LocationCode").val();
+        var assetCode = $("AssetCode").val();
+        alert("L: " + locationCode + ", A:" + assetCode);
+        var data = new AssignLocationModel();
+        data.LocationCode = locationCode;
+        data.AssetCode = assetCode;
+        //lähetetään JSON-muotoista dataa palvelimelle
+        $.ajax({
+            type: "POST",
+            url: "/Asset/AssignLocation",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (data) {
+                if (data.success == true) {
+                    alert("Asset successfully assigned");
+                }
+                else {
+                    alert("succ error: " + data.error);
+                }
+            },
+            dataType: "json"
+        });
     });
 }
 //# sourceMappingURL=Logic.js.map
--- AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.ts
@@ -1,7 +1,36 @@
 ﻿/// <reference path="../typings/jquery/jquery.d.ts" />
 
+class AssignLocationModel {
+	public AssetCode: string;
+	public LocationCode: string;
+}
+
 function initAssetAssignment() {
 	$("#AssignAssetButton").click(function () {
-		alert("Jdenfrei");
+
+		var locationCode: string = $("LocationCode").val();
+		var assetCode: string = $("AssetCode").val();
+
+		alert("L: " + locationCode + ", A:" + assetCode);
+		var data: AssignLocationModel = new AssignLocationModel();
+		data.LocationCode = locationCode;
+		data.AssetCode = assetCode;
+
+		//lähetetään JSON-muotoista dataa palvelimelle
+		$.ajax({
+			type: "POST",
+			url: "/Asset/AssignLocation",
+			data: JSON.stringify(data),
+			contentType: "application/json",
+			success: function (data) {
+				if (data.success == true) {
+					alert("Asset successfully assigned");
+				}
+				else {
+					alert("succ error: " + data.error);
+					}
+				},
+			dataType: "json"
+		});
 	});
 }
