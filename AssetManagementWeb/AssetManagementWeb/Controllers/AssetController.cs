97c1300d4948e9df9bb8c9d555dab232fb8e95e5
Jalmari Talvipuro <jalmari.talvipuro@stud.pointcollege.fi>
2018-12-16 18:23:43 +0200

    Globalization

--- AssetManagementWeb/AssetManagementWeb/Controllers/AssetController.cs
+++ AssetManagementWeb/AssetManagementWeb/Controllers/AssetController.cs
@@ -24,17 +24,38 @@ namespace AssetManagementWeb.Controllers
             return View();
         }
 
-        // GET: Asset/Details/5
-        public ActionResult Details(int id)
+        public ActionResult List()
         {
-            return View();
+            List<LocatedAssetsViewModel> model = new List<LocatedAssetsViewModel>();
+
+            AssetManagementEntities1 entities = new AssetManagementEntities1();
+            try
+            {
+                List<AssetLocation1> assets = entities.AssetLocations1.ToList();
+
+                //muodostetaan näkymämalli tietokannan rivien pohjalta
+                CultureInfo fiFI = new CultureInfo("fi-FI");
+                foreach (AssetLocation1 asset in assets)
+                {
+                    LocatedAssetsViewModel view = new LocatedAssetsViewModel();
+                    view.Id = asset.Id;
+                    view.LocationCode = asset.AssetLocation.Code;
+                    view.LocationName = asset.AssetLocation.Name;
+                    view.AssetCode = asset.Asset.Code;
+                    view.AssetName = asset.Asset.Type + ": "+asset.Asset.Model;
+                    view.LastSeen = asset.LastSeen.Value.ToString(fiFI);
+
+                    model.Add(view);
+                }
+            }
+            finally
+            {
+                entities.Dispose();
+            }
+
+                return View(model);
         }
 
-        // GET: Asset/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
         [HttpPost]
         public JsonResult AssignLocation()
@@ -84,66 +105,6 @@ namespace AssetManagementWeb.Controllers
             // palautetaan JSON-muotoinen tulos kutsujalle
             var result = new { success = success, error = error };
             return Json(result);
-        }
-
-        // POST: Asset/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Asset/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Asset/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Asset/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Asset/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
---
+++ AssetManagementWeb/AssetManagementWeb/Models/LocatedAssetsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssetManagementWeb.Models
+{
+    public class LocatedAssetsViewModel
+    {
+        public int Id { get; set; }
+        public string LocationCode { get; set; }
+        public string LocationName { get; set; }
+        public string AssetCode { get; set; }
+        public string AssetName { get; set; }
+        public string LastSeen { get; set; }
+    }
+}
